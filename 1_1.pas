2b1cfe28e078bae9cb0782d8adb3cee224b3fc9d
Anytkaa <144678499+Anytkaa@users.noreply.github.com>
2023-11-01 18:36:20 +0300

    Add files via upload

---
+++ 1_1.pas
@@ -0,0 +1,25 @@
+﻿program zadanie_1;
+
+var
+  arr: array[1..20] of integer;
+  i: integer;
+
+begin
+  writeln('Введите 20 целых чисел');
+  for i := 1 to 20 do
+  begin
+    readln(arr[i]);
+  end;
+  for i := 1 to 20 do
+  begin
+    if arr[i] > 0 then
+      arr[i] := 0
+    else
+      arr[i] := arr[i] * arr[i];
+  end;
+  writeln('Измененный массив: ');
+  for i := 1 to 20 do
+  begin
+    writeln(arr[i]);
+  end;
+end.
