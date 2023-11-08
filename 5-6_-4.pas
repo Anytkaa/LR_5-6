1146e58cac9ea2da8bcb4557c7c91f245c9e7b6c
Anytkaa <144678499+Anytkaa@users.noreply.github.com>
2023-11-08 19:49:41 +0300

    Add files via upload

---
+++ 5-6_-4.pas
@@ -0,0 +1,13 @@
+﻿var a:array[1..30] of integer;
+i:integer;
+begin
+for i:=1 to 30 do
+begin
+a[ i ]:=random(167)-99;//массив от -99 до 67
+write(a[ i ],' ');
+end;
+writeln;//переход к следующей строке
+for i:=1 to 30 do
+if a [ i ]mod 2 = 0 then//этот же массив но элементы четные
+write (a[i],' ');
+end.
---
+++ 5-6_-6.pas
@@ -0,0 +1,29 @@
+﻿var
+a: array[1..10] of Integer;
+i: Integer;
+ord: Boolean;
+begin
+// Заполняем массив
+for i := 1 to 10 do
+begin
+Write('Введите элемент массива a[', i, ']: ');
+Read(a[i]);
+end;
+
+// Проверяем упорядоченность массива
+ord := True;
+for i := 2 to 10 do
+begin
+if a[i] < a[i-1] then
+begin
+ord := False;
+Break;
+end;
+end;
+
+// Выводим результат
+if ord=true then
+WriteLn('Массив упорядочен по возрастанию.')
+else
+WriteLn('Массив не упорядочен по возрастанию.');
+end.
